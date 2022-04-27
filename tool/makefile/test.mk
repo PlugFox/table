98b8bbc6e58f17b56521b95f41590b1e59dc0121
Plague Fox <plugfox@gmail.com>
2022-04-27 12:11:52 +0400

    test in pipeline

--- tool/makefile/test.mk
+++ tool/makefile/test.mk
@@ -4,7 +4,6 @@ test: get
 	@flutter test --no-pub --concurrency=6 --coverage test/all_test.dart
 
 coverage: test
-	@dart run coverage:format_coverage --lcov --in=coverage --out=coverage/lcov.info --packages=.packages --report-on=lib
 	@genhtml -o coverage coverage/lcov.info
 
 check:
